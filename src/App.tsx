a3dc3e6a75bc75e4398c8c68662e47af1a41f7b4
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-10-22 19:20:13 +0600

    delete browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Login from './pages/login/Login'
 import Registration from './pages/registration/Registration'
 import Header from './components/header/Header';
