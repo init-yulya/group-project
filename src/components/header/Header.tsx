664f55087a95a9aeff57371f0d536136267ae3a3
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-10-28 01:18:48 +0600

    edit header`s style

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,15 +2,13 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import { Store } from '../../store/store.types';
 import { useAppDispatch } from '../../store/store';
 import { clearUser, logoutUser } from '../../store/userSlice';
@@ -51,108 +49,46 @@ export default function Header() {
   };
 
   return (
-    <AppBar position="static">
-      <Container maxWidth="xl" sx={{ backgroundColor: 'black' }}>
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            YandexLogo
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page.id} onClick={handleCloseNavMenu}>
-                  <Typography
-                    to={page.path}
-                    component={Link}
-                    sx={{
-                      mr: 2,
-                      display: { xs: 'flex', md: 'none' },
-                      flexGrow: 1,
-                      color: 'inherit',
-                      textDecoration: 'none',
-                    }}
-                  >
-                    {page.name}
-                  </Typography>
-                </MenuItem>
-              ))}
-              <MenuItem onClick={onLogout}>
-                <span>Выйти</span>
-              </MenuItem>
-            </Menu>
-          </Box>
-
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              color: 'inherit',
-              textDecoration: 'none',
-              letterSpacing: '.3rem',
-            }}
-          >
-            YandexLogo
-          </Typography>
-
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+    <AppBar position="static" sx={{ backgroundColor: '#1A1B22', height: '60px' }}>
+      <Container maxWidth="xl">
+        <Toolbar>
+          <MenuItem component={Link} to="/">
+            <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="44" height="44" fill="#1A1B22" />
+              <rect width="2.64" height="18" transform="matrix(1 0 0 -1 12 31)" fill="#F9FAFB" />
+              <rect width="2.64" height="18" transform="matrix(1 0 0 -1 29 31)" fill="#F9FAFB" />
+              <rect width="2.64" height="5" transform="matrix(0 -1 -1 0 21.3203 21.3198)" fill="#F9FAFB" />
+              <rect width="2.64" height="5" transform="matrix(0 -1 -1 0 27.3203 24.3198)" fill="#F9FAFB" />
+            </svg>
+          </MenuItem>
+          <Box sx={{ flexGrow: 1, display: { md: 'inline-flex' } }}>
             {pages.map((page) => (
               <Button
                 key={page.id}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ textTransform: 'none', color: '#B5B5B7', p: '20px' }}
                 component={Link}
                 to={page.path}
               >
                 {page.name}
               </Button>
             ))}
-            <MenuItem onClick={onLogout}>
-              <span>Выйти</span>
+            <Box sx={{ flexGrow: 1 }} />
+            <MenuItem>
+              <IconButton color="secondary" component={Link} to="/">
+                <AccountCircleOutlinedIcon />
+              </IconButton>
+            </MenuItem>
+            <MenuItem>
+              <Button
+                variant="outlined"
+                onClick={onLogout}
+                sx={{
+                  textTransform: 'none', color: 'white', borderColor: 'white', fontSize: '14px', borderRadius: '6',
+                }}
+              >
+                Выйти
+              </Button>
             </MenuItem>
           </Box>
         </Toolbar>
