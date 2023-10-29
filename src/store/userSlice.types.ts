6723381d047c6625463d10234033e5a9ea5c5b5b
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-10-29 21:24:41 +0600

    edit types

--- src/store/userSlice.types.ts
+++ src/store/userSlice.types.ts
@@ -1,25 +1,26 @@
 export interface IUser {
   id?: number
-  login: string
-  first_name: string
-  second_name: string
-  display_name?: string
-  email: string
-  avatar?: string
-  phone: string
+  email: string;
+  first_name: string;
+  last_name: string;
+  telegram?: string;
+  phone_number?: string;
+  company?: string;
+  password: string;
 }
 
 export interface RegisterRequestData {
-  first_name: string
-  second_name: string
-  login: string
-  email: string
-  password: string
-  phone: string
+  email: string;
+  first_name: string;
+  last_name: string;
+  telegram: string;
+  phone_number: string;
+  company: string;
+  password: string;
 }
 
 export interface LoginRequestData {
-  login: string
+  email: string
   password: string
 }
 
