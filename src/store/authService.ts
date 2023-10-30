395a37c5523093edbdb13870d09e364d23346f26
Yulia Gladysheva <yulliagladysheva@gmail.com>
2023-10-30 23:16:47 +0600

    fix types

--- src/store/authService.ts
+++ src/store/authService.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { RootState } from './store';
 
